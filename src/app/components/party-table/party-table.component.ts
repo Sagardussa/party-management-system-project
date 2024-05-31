3f5d25745c559167ff11f349b21fde01fd26232e
Sagardussa <sagardussa8004@gmail.com>
2024-05-31 11:50:44 +0530

    first commit

---
+++ src/app/components/party-table/party-table.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { PartyStore } from '../store/party.store';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-party-table',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './party-table.component.html',
+  styleUrl: './party-table.component.scss',
+})
+export class PartyTableComponent implements OnInit {
+  partyStore = inject(PartyStore);
+  parties$ = this.partyStore.loadPartyListAll();
+  router = inject(Router);
+  toastrService = inject(ToastrService);
+
+
+  ngOnInit(): void {
+    this.parties$ = this.partyStore.loadPartyListAll();
+  }
+
+  redirectToEditPArtyForm(id: any) {
+    this.router.navigate(['party-list', id]);
+  }
+
+  deleteParty(id: number) {
+    this.partyStore.deletePartyList(id);
+    this.toastrService.success('deleted completed successfully!', 'Success');
+
+  }
+}
